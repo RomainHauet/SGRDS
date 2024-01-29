d5b1f12f27b3b146acdd21177e2bea9385d39840
WD-Toaster <zorg.tassery@gmail.com>
2024-01-29 14:41:03 +0100

    css first

--- app/Views/page_connexion.php
+++ app/Views/page_connexion.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Page de connexion</title>
+    <link href="/assets/css/connexion.css" rel="stylesheet" type="text/css">
 </head>
 <body>
 
@@ -11,13 +12,15 @@
 
     <?php echo form_open('connexion'); ?>
 
-    <label for="username">Identifiant:</label>
-    <input type="text" name="username" required>
+    <div id="cent">
+        <label for="username">Identifiant:</label>
+        <input type="text" name="username" required>
 
-    <label for="password">Mot de passe:</label>
-    <input type="password" name="password" required>
+        <label for="password">Mot de passe:</label>
+        <input type="password" name="password" required>
 
-    <button type="submit">Connexion</button>
+        <button type="submit">Connexion</button>
+    </div>
 
     <?php echo form_close(); ?>
 
