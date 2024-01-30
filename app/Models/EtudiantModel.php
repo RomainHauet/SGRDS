267583110d82b3b644595386f9bb5a5f27805f89
RomainHauet <romain.hauet@etu.univ-lehavre.fr>
2024-01-30 14:17:16 +0100

    Mise à jour des model

--- app/Models/EtudiantModel.php
+++ app/Models/EtudiantModel.php
@@ -1,14 +1,23 @@
 <?php
-	namespace App\Models;
-	use CodeIgniter\Model;
-	class EtudiantModel extends Model
-	{
-		protected $table = 'etudiants';
-		protected $primaryKey = 'id';
 
-		protected $allowedFields = [
-		'nom',
-		'prenom',
-		'email',
-		];
-	}
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class EtudiantModel extends Model
+{
+    protected $table = 'rattrapage';
+    protected $primaryKey = 'id_Edt';
+    
+    protected $allowedFields = [
+        'nom',
+        'prenom',
+        'email',
+    ];
+
+    protected $validationRules = [
+        'nom' => 'required',
+        'prenom' => 'required',
+        'email' => 'required',
+    ];
+}
