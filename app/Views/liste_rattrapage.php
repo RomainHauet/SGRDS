ce41ee4e24f67a2180e66f40dbb433c52ca60f0b
WD-Toaster <zorg.tassery@gmail.com>
2024-01-30 14:07:48 +0100

    commit

--- app/Views/liste_rattrapage.php
+++ app/Views/liste_rattrapage.php
@@ -4,45 +4,47 @@
         <meta charset="UTF-8">
         <title>Liste des Élèves</title>
         <link href="/assets/css/ListeEtudiant.css" rel="stylesheet" type="text/css">
-        <script src="<?= base_url('assets/js/ListeEtudiant.js');?>" type="text/javascript"></script>
+        <script src="<?=base_url('assets/js/ListeEtudiant.js');?>" type="text/javascript"></script>
     </head>
     <body>
         <h1>Liste des Élèves</h1>
         <?php if (!empty($rattrapages)): ?>
         <div class="pager">
         <table border="1" class="sortable">
-            <tr>
-                <th>
-                    <button>
-                        Ressource
-                        <span aria-hidden="true"></span>
-                    </button>
-                </th>
-                <th>
-                    <button>
-                        Date
-                        <span aria-hidden="true"></span>
-                    </button>
-                </th>
-                <th>
-                    <button>
-                        Durée
-                        <span aria-hidden="true"></span>
-                    </button>
-                </th>
-                <th>
-                    <button>
-                        Enseignant
-                        <span aria-hidden="true"></span>
-                    </button>
-                </th>
-                <th>
-                    <button>
-                        Etat
-                        <span aria-hidden="true"></span>
-                    </button>
-                </th>
-            </tr>
+            <thead>
+                <tr>
+                    <th aria-sort="ascending">
+                        <button>
+                            Ressource
+                            <span aria-hidden="true"></span>
+                        </button>
+                    </th>
+                    <th>
+                        <button>
+                            Date
+                            <span aria-hidden="true"></span>
+                        </button>
+                    </th>
+                    <th>
+                        <button>
+                            Durée
+                            <span aria-hidden="true"></span>
+                        </button>
+                    </th>
+                    <th>
+                        <button>
+                            Enseignant
+                            <span aria-hidden="true"></span>
+                        </button>
+                    </th>
+                    <th>
+                        <button>
+                            Etat
+                            <span aria-hidden="true"></span>
+                        </button>
+                    </th>
+                </tr>
+            </thead>
             <?php foreach ($rattrapages as $rattrapage): ?>
                 <tr>
                     <td><?=$rattrapage['ressource'];?></td>
