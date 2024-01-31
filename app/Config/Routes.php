a5b0713f79bdfd289b201494b98afc531fc8f77b
Liam <liam.deniau@etu.univ-lehavre.fr>
2024-01-31 11:06:10 +0100

    Modif de rattrapage

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -9,6 +9,10 @@ use CodeIgniter\Router\RouteCollection;
 $routes->get('/connexion', 'ConnexionController::index');
 $routes->get('/deconnexion', 'ConnexionController::deconnexion');
 $routes->post('/connexion', 'ConnexionController::loginAuth');
+$routes->get('/modifier/(:num)', 'ListeRattrapageController::modifier/$1');
+$routes->post('/modifier/(:num)', 'ListeRattrapageController::modifierRattrapage/$1');
+$routes->get('/supprimer/(:num)', 'ListeRattrapageController::supprimer/$1');
+$routes->post('/supprimer/(:num)', 'ListeRattrapageController::supprimerRattrapage/$1');
 
 $routes->get('/', 'ListeRattrapageController::index');
 
--- app/Views/ajout_rattrapage.php
+++ app/Views/ajout_rattrapage.php
@@ -6,18 +6,19 @@
     </head>
     <body>
         <h1>Ajout d'un rattrapage</h1>
-        <form action="/ajout" method="post">
+        <form action="<?php echo isset($rattrapage) ? '/modifier/'.$rattrapage['id_R'] : '/ajout'; ?>" method="post">
+
             <label for="ressource">Ressource</label>
-            <input type="text" name="ressource" id="ressource" required>
+            <input type="text" name="ressource" id="ressource" <?php if(isset($rattrapage)) echo 'value="'.$rattrapage['ressource'].'" '; ?> required>
             <br>
-            <label for="date">Date</label>
-            <input type="date" name="date" id="date" required>
+            <label for="date_DS">Date</label>
+            <input type="date" name="date_DS" id="date_DS" <?php if(isset($rattrapage)) echo 'value="'.$rattrapage['date_DS'].'"'; ?> required>
             <br>
             <label for="duree">Durée</label>
-            <input type="number" name="duree" id="duree" required>
+            <input type="number" name="duree" id="duree" <?php if(isset($rattrapage)) echo 'value="'.$rattrapage['duree'].'"'; ?> required>
             <br>
             <label for="enseignant">Enseignant</label>
-            <input type="text" name="enseignant" id="enseignant" required>
+            <input type="text" name="enseignant" id="enseignant" <?php if(isset($rattrapage)) echo 'value="'.$rattrapage['enseignant'].'"'; ?> required>
             <br>
             <input type="submit" value="Ajouter">
         </form>
