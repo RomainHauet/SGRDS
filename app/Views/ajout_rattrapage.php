2a3e2f8c75e21789d8a535e29312dce4016346c8
RomainHauet <romain.hauet@etu.univ-lehavre.fr>
2024-01-31 15:37:45 +0100

    TY

--- app/Views/ajout_rattrapage.php
+++ app/Views/ajout_rattrapage.php
@@ -48,43 +48,34 @@
         <a href="/">Retour à la liste des rattrapages</a>
         
         <script>
-            console.log('Je rentre dans le script');
-            // Récupère les semestres
-            const semestres = <?= json_encode($semestres) ?>;
-
-            // Trie les semestres par ordre croissant
-            semestres.sort();
-
-            // Récupère semestre sélectionné du formulaire
-            const semestre = document.getElementById('semestre');
-
-            // Affiche les ressources correspondantes au semestre sélectionné
-            
+            // Au changement de valeur du semestre
             semestre.addEventListener('change', () =>
             {
-                console.log('Je rentre dans le change');
-                // Récupère la ressource sélectionnée selon le semestre
-                const ressources = <?= json_encode($ressources) ?>;
+                // Je récupère la valeur du semestre sélectionné
+                const semestre = document.getElementById('semestre').value;
+
+                // Je récupère la liste des ressources
+                const ressources = <?php echo json_encode($ressources); ?>;
+
+                // Je récupère le select des ressources
                 const ressource = document.getElementById('ressource');
                 ressource.innerHTML = '';
-                for (let i = 0; i < ressources.length; i++)
+
+                // Je parcours la liste des ressources
+                for(let i = 0; i < ressources.length; i++)
                 {
-                    console.log(semestre.value[1], ressources[i][2]);
-                    // Je vérifie si la 2ème lettre du semestre correspond à la 2ème lettre du semestre de la ressource
-                    if (semestre.value[1] === ressources[i][2])
+                    // Si la ressource correspond au semestre sélectionné
+                    if(ressources[i].split(',')[0] === semestre)
                     {
-                        console.log('Je rentre dans le if');
-                        // Si oui, j'ajoute la ressource à la liste
+                        // Je crée une option avec le nom de la ressource
                         const option = document.createElement('option');
-                        option.value = ressources[i];
-                        option.textContent = ressources[i];
+                        option.value = ressources[i].id_R;
+                        option.innerHTML = ressources[i].nom;
+                        // J'ajoute l'option au select
                         ressource.appendChild(option);
                     }
                 }
-                console.log('Je sors du change');
             });
-            console.log('Finished');
         </script>
     </body>
-
 </html>
