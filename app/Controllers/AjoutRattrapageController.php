f4baf5cbef06b0ed4ed9ba667c67d657a4abcfb2
WD-Toaster <zorg.tassery@gmail.com>
2024-01-31 17:03:29 +0100

    indent

--- app/Controllers/AjoutRattrapageController.php
+++ app/Controllers/AjoutRattrapageController.php
@@ -2,9 +2,9 @@
 
 namespace App\Controllers;
 
+use App\Models\EnseignantModel;
 use App\Models\RattrapageModel;
 use App\Models\SemestreModel;
-use App\Models\EnseignantModel;
 
 class AjoutRattrapageController extends BaseController
 {
@@ -13,12 +13,12 @@ class AjoutRattrapageController extends BaseController
         // Verifie que l'utilisateur est connecté sinon il le redirige vers la page de connexion
         /*if(!session()->get('isLoggedIn'))
         {
-            helper(['form']);
-            echo view('ajout_rattrapage');
+        helper(['form']);
+        echo view('ajout_rattrapage');
         }
         else
         {
-            return redirect()->to('/connexion');
+        return redirect()->to('/connexion');
         }*/
 
         // Récupère les semestres et toutes les ressources
@@ -51,34 +51,34 @@ class AjoutRattrapageController extends BaseController
         // Le rattrapage est ajouté dans la base de données
         $model->save($data);
 
-            $session = session();
-            $session->setFlashdata('success', 'Rattrapage ajouté avec succès');
+        $session = session();
+        $session->setFlashdata('success', 'Rattrapage ajouté avec succès');
 
-            //Envoi d'un mail à l'enseignant
+        //Envoi d'un mail à l'enseignant
 
-            $enseignantModel = new EnseignantModel();
-            $enseignant = $enseignantModel->where('id_E', $data['enseignant'])->first();
-            $emailService = \Config\Services::email();
+        $enseignantModel = new EnseignantModel();
+        $enseignant = $enseignantModel->where('id_E', $data['enseignant'])->first();
+        $emailService = \Config\Services::email();
 
-            $message = " Bonjour". $enseignant['prenom']." ". $enseignant['nom'] .",
-            Un rattrapage a été ajouté à votre emploi du temps le ". $data['date'] ." de ". $data['duree'] ." heure(s). 
+        $message = " Bonjour" . $enseignant['prenom'] . " " . $enseignant['nom'] . ",
+            Un rattrapage a été ajouté à votre emploi du temps le " . $data['date'] . " de " . $data['duree'] . " heure(s).
             Merci de valider ou de refuser ce rattrapage sur le site de gestion des rattrapages de l'IUT.";
 
-            $from = 'tassery.hugo@gmail.com';
-            $to = $enseignant['email'];
-            $subject = 'Ajout d\'un rattrapage le '. $data['date'];
+        $from = 'tassery.hugo@gmail.com';
+        $to = $enseignant['email'];
+        $subject = 'Ajout d\'un rattrapage le ' . $data['date'];
 
-            $emailService->setTo($to);
-            $emailService->setFrom($from);
-            $emailService->setSubject($subject);
-            $emailService->setMessage($message);
-            if ($emailService->send()) {
-                echo 'E-mail envoyé avec succès.';
-            } else {
-                echo $emailService->printDebugger();
-            }
+        $emailService->setTo($to);
+        $emailService->setFrom($from);
+        $emailService->setSubject($subject);
+        $emailService->setMessage($message);
+        if ($emailService->send()) {
+            echo 'E-mail envoyé avec succès.';
+        } else {
+            echo $emailService->printDebugger();
+        }
 
-            return redirect()->to('/');
-        
+        return redirect()->to('/');
+
     }
 }
