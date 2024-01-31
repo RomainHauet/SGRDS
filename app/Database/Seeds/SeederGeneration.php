1d112d057d1181189e508bd792b6abeff0dd7c53
RomainHauet <romain.hauet@etu.univ-lehavre.fr>
2024-01-31 09:10:07 +0100

    Ajout De participeSeeder au à la generation

--- app/Database/Seeds/SeederGeneration.php
+++ app/Database/Seeds/SeederGeneration.php
@@ -12,7 +12,6 @@ class SeederGeneration extends Seeder
         $this->call('DirecteurSeeder');
         $this->call('EtudiantSeeder');
         $this->call('EnseignantSeeder');
-        // $this->call('ParticipeSeeder');
-
+        $this->call('ParticipeSeeder');
     }
 }
