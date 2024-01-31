72a175bbcd5f037c29620a0a81ecd447ad682fc7
WD-Toaster <zorg.tassery@gmail.com>
2024-01-31 17:01:53 +0100

    mailProf

--- app/Controllers/AjoutRattrapageController.php
+++ app/Controllers/AjoutRattrapageController.php
@@ -4,6 +4,7 @@ namespace App\Controllers;
 
 use App\Models\RattrapageModel;
 use App\Models\SemestreModel;
+use App\Models\EnseignantModel;
 
 class AjoutRattrapageController extends BaseController
 {
@@ -56,7 +57,31 @@ class AjoutRattrapageController extends BaseController
 
             $session = session();
             $session->setFlashdata('success', 'Rattrapage ajouté avec succès');
-            
+
+            //Envoi d'un mail à l'enseignant
+
+            $enseignantModel = new EnseignantModel();
+            $enseignant = $enseignantModel->where('id_E', $data['enseignant'])->first();
+            $emailService = \Config\Services::email();
+
+            $message = " Bonjour". $enseignant['prenom']." ". $enseignant['nom'] .",
+            Un rattrapage a été ajouté à votre emploi du temps le ". $data['date'] ." de ". $data['duree'] ." heure(s). 
+            Merci de valider ou de refuser ce rattrapage sur le site de gestion des rattrapages de l'IUT.";
+
+            $from = 'tassery.hugo@gmail.com';
+            $to = $enseignant['email'];
+            $subject = 'Ajout d\'un rattrapage le '. $data['date'];
+
+            $emailService->setTo($to);
+            $emailService->setFrom($from);
+            $emailService->setSubject($subject);
+            $emailService->setMessage($message);
+            if ($emailService->send()) {
+                echo 'E-mail envoyé avec succès.';
+            } else {
+                echo $emailService->printDebugger();
+            }
+
             return redirect()->to('/');
         }
         else
